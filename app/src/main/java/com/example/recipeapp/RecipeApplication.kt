a45de3ef05e5e261a499138fc81145053347b534
Nito <german220291@gmail.com>
2021-04-01 10:06:08 +0100

    Added Constant.kt and api key from secrets

--- app/src/main/java/com/example/recipeapp/RecipeApplication.kt
+++ app/src/main/java/com/example/recipeapp/RecipeApplication.kt
@@ -1,7 +1,21 @@
 package com.example.recipeapp
 
 import android.app.Application
+import android.content.res.Resources
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class RecipeApplication : Application() {}
+class RecipeApplication : Application() {
+
+    companion object {
+        lateinit var instance: Application
+        lateinit var res: Resources
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        instance = this
+        res = resources
+    }
+}
---
+++ app/src/main/java/com/example/recipeapp/utils/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.recipeapp.utils
+
+import com.example.recipeapp.R
+import com.example.recipeapp.RecipeApplication
+
+class Constants {
+
+    companion object {
+
+        val API_KEY = RecipeApplication.res.getString(R.string.api_key)
+    }
+}
